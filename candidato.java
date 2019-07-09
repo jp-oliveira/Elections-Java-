623a67a32edd1cae83c022c121d57e347866a93d
João Pedro Oliveira <49132333+jp-oliveira@users.noreply.github.com>
2019-07-09 12:09:19 -0300

    Add files via upload

---
+++ candidato.java
@@ -0,0 +1,44 @@
+package com.pac.elections.Modelo;
+
+import java.io.Serializable;
+
+public class candidato implements Serializable {
+
+    String nome;
+    int numero;
+    String partido;
+    int votos;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    public String getPartido() {
+        return partido;
+    }
+
+    public void setPartido(String partido) {
+        this.partido = partido;
+    }
+
+    public int getVotos() {
+        return votos;
+    }
+
+    public void setVotos(int votos) {
+        this.votos = votos;
+    }
+
+}
